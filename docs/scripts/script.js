7f2b776bb893a7f1360f7e06b91e8b59e6a002c2
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2025-01-13 04:43:58 +0700

    Update script.js

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -51,18 +51,10 @@ new Vue({
       let dursec = Math.floor(this.audio.duration - durmin * 60);
       let curmin = Math.floor(this.audio.currentTime / 60);
       let cursec = Math.floor(this.audio.currentTime - curmin * 60);
-      if (durmin < 10) {
-        durmin = "0" + durmin;
-      }
-      if (dursec < 10) {
-        dursec = "0" + dursec;
-      }
-      if (curmin < 10) {
-        curmin = "0" + curmin;
-      }
-      if (cursec < 10) {
-        cursec = "0" + cursec;
-      }
+      durmin = durmin < 10 ? "0" + durmin : durmin;
+      dursec = dursec < 10 ? "0" + dursec : dursec;
+      curmin = curmin < 10 ? "0" + curmin : curmin;
+      cursec = cursec < 10 ? "0" + cursec : cursec;
       this.duration = durmin + ":" + dursec;
       this.currentTime = curmin + ":" + cursec;
     },
@@ -71,12 +63,7 @@ new Vue({
       let maxduration = this.audio.duration;
       let position = x - progress.offsetLeft;
       let percentage = (100 * position) / progress.offsetWidth;
-      if (percentage > 100) {
-        percentage = 100;
-      }
-      if (percentage < 0) {
-        percentage = 0;
-      }
+      percentage = Math.max(0, Math.min(percentage, 100));
       this.barWidth = percentage + "%";
       this.circleLeft = percentage + "%";
       this.audio.currentTime = (maxduration * percentage) / 100;
@@ -90,22 +77,20 @@ new Vue({
     prevTrack() {
       this.transitionName = "scale-in";
       this.isShowCover = false;
-      if (this.currentTrackIndex > 0) {
-        this.currentTrackIndex--;
-      } else {
-        this.currentTrackIndex = this.tracks.length - 1;
-      }
+      this.currentTrackIndex = 
+        this.currentTrackIndex > 0 
+        ? this.currentTrackIndex - 1 
+        : this.tracks.length - 1;
       this.currentTrack = this.tracks[this.currentTrackIndex];
       this.resetPlayer();
     },
     nextTrack() {
       this.transitionName = "scale-out";
       this.isShowCover = false;
-      if (this.currentTrackIndex < this.tracks.length - 1) {
-        this.currentTrackIndex++;
-      } else {
-        this.currentTrackIndex = 0;
-      }
+      this.currentTrackIndex = 
+        this.currentTrackIndex < this.tracks.length - 1 
+        ? this.currentTrackIndex + 1 
+        : 0;
       this.currentTrack = this.tracks[this.currentTrackIndex];
       this.resetPlayer();
     },
@@ -114,16 +99,16 @@ new Vue({
       this.circleLeft = 0;
       this.audio.currentTime = 0;
       this.audio.src = this.currentTrack.source;
+      this.audio.load();
       setTimeout(() => {
-        if(this.isTimerPlaying) {
+        if (this.isTimerPlaying) {
           this.audio.play();
-        } else {
-          this.audio.pause();
         }
       }, 300);
     },
     favorite() {
-      this.tracks[this.currentTrackIndex].favorited = !this.tracks[ this.currentTrackIndex ].favorited;
+      this.tracks[this.currentTrackIndex].favorited = 
+        !this.tracks[this.currentTrackIndex].favorited;
     }
   },
   created() {
@@ -131,30 +116,32 @@ new Vue({
     this.currentTrack = this.tracks[0];
     this.audio = new Audio();
     this.audio.src = this.currentTrack.source;
-    this.audio.ontimeupdate = function() {
+
+    this.audio.ontimeupdate = function () {
       vm.generateTime();
     };
-    this.audio.onloadedmetadata = function() {
+    this.audio.onloadedmetadata = function () {
       vm.generateTime();
     };
-    this.audio.onended = function() {
+    this.audio.onended = function () {
       vm.nextTrack();
-      this.isTimerPlaying = true;
+      vm.isTimerPlaying = true;
     };
 
-    // this is optional (for preload covers)
-    for (let index = 0; index < this.tracks.length; index++) {
-      const element = this.tracks[index];
+    this.audio.onerror = function () {
+      console.error("Failed to load audio source:", vm.audio.src);
+    };
+
+    // Prefetch cover images
+    for (let track of this.tracks) {
       let link = document.createElement('link');
       link.rel = "prefetch";
-      link.href = element.cover;
-      link.as = "image"
-      document.head.appendChild(link)
+      link.href = track.cover;
+      link.as = "image";
+      document.head.appendChild(link);
     }
   },
   mounted() {
-    // เมื่อ Vue instance ถูก mounted ให้เริ่มเล่นเพลง
-    this.audio.play();
-    this.isTimerPlaying = true;
+    console.log("Audio initialized:", this.audio.src);
   }
 });
